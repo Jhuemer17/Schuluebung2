55a787d32ff008ca0b9ab91dee60fb34c07111d3
Jhuemer17 <johannes.huemer@gmx.at>
2019-09-24 11:31:45 +0200

    commit 4 (Beispiel3)

--- Beispiel3/src/NumberTester.java
+++ Beispiel3/src/NumberTester.java
@@ -8,43 +8,52 @@ import java.io.IOException;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Johannes Huemer
  */
 public class NumberTester {
-    public String pfad;
+
+    private String pfad;
+    
+    private NumberTest oddEvenTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
 
     public NumberTester(String filename) {
         this.pfad = pfad;
-        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
-        {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddEvenTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+    }
+
+    public void testFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String x = br.readLine();
-            for(int i = 0; i< Integer.parseInt(x);i++)
-            {
-                String[]line = br.readLine().split(" ");
+            for (int i = 0; i < Integer.parseInt(x); i++) {
+                String[] line = br.readLine().split(" ");
                 int index = Integer.parseInt(line[0]);
-                
-                switch(index)
-                {
+
+                switch (index) {
                     case 1:
                         
                 }
             }
-        }
-        catch(IOException ex)
-        {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void setOddEvenTester(NumberTest oddTester){}
-    public void setPrimeTester(NumberTest primeTester){}
-    public void setPalindromeTester(NumberTest palindromeTester){}
-    public void testFile(){}
-    
-    public interface NumberTest{
+    public interface NumberTest {
+
         boolean testNumber(int number);
     }
 }
